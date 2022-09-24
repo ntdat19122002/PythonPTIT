ae687f8c091d1df53ffd28a9ab442e0109e117ca
Your Name <you@example.com>
2022-09-25 04:35:35 +0700

    So may man

---
+++ SoMayMan2.py
@@ -0,0 +1,13 @@
+def check(n):
+    for i in range(len(n)):
+        if (n[i] != '4' and n[i] != '7'):
+            return False
+    return True
+
+T = int(input())
+for t in range(T):
+    n = input()
+    if(check(n)):
+        print('YES')
+    else:
+        print('NO')
